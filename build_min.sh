6789ef031e835906e94e4e150317e134dd2e292b
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-03 00:59:25 +0900

    Update build_min.sh

--- build_min.sh
+++ build_min.sh
@@ -6,21 +6,23 @@ export _VER_YOURS=${_VER_YOURS:-0.3}
 export _BUILDPACKDEPS_TAG_FOR_LATEST=22.10
 export _FFMPEG_VERSION=4.4.2
 export _OPENCV_VERSION=3.4.15
+cd min
 for _BUILDPACKDEPS_TAG in 22.04 22.10 ; do
-(
+
     export _FFMPEG_EXTRA_VERSION_SUFFIX=${_YOU}${_VER_MIN}-min
-    cd min
     docker build -f Dockerfile -t ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
            --build-arg _BUILDPACKDEPS_TAG=${_BUILDPACKDEPS_TAG} \
            --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
            --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
            .
+    test $? -ne 0 || exit $?
     docker tag \
            ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
            ${_YOU}/ffmpeg-yours-min:latest-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}
     if \! -z "${__dopush}" ; then
-        docker push ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}
+        docker push ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} && \
         docker push ${_YOU}/ffmpeg-yours-min:latest-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}
+        test $? -ne 0 || exit $?
     fi
     if test "${_BUILDPACKDEPS_TAG}" = "${_BUILDPACKDEPS_TAG_FOR_LATEST}" ; then
         docker tag \
@@ -28,7 +30,8 @@ for _BUILDPACKDEPS_TAG in 22.04 22.10 ; do
                ${_YOU}/ffmpeg-yours-min:latest
         if \! -z "${__dopush}" ; then
             docker push ${_YOU}/ffmpeg-yours-min:latest
+            test $? -ne 0 || exit $?
         fi
     fi
-)
+
 done
