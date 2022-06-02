3513debc58314ddfb48a5aba747a7db03cf54a7f
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-03 00:31:08 +0900

    Update build_yours.sh

--- build_yours.sh
+++ build_yours.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+export __dopush=$(test "z${1}" = "z--push" && echo 1)
 export _YOU=${_YOU:-hhsprings}
 export _VER_MIN=0.3
 export _VER_YOURS=0.3
@@ -14,11 +15,17 @@ for _BUILDPACKDEPS_TAG in 22.04 22.10 ; do
            --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
            --build-arg _OPENCV_VERSION=${_OPENCV_VERSION} \
            --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
+    if \! -z "${__dopush}" ; then
+        docker push ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_OPENCV_VERSION}-${_BUILDPACKDEPS_TAG}
+    fi
            .
     if test "${_BUILDPACKDEPS_TAG}" = "${_BUILDPACKDEPS_TAG_FOR_LATEST}" ; then
         docker tag \
                ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_OPENCV_VERSION}-${_BUILDPACKDEPS_TAG} \
                ${_YOU}/ffmpeg-yours:latest
+        if \! -z "${__dopush}" ; then
+            docker push ${_YOU}/ffmpeg-yours:latest
+        fi
     fi
 )
 done
