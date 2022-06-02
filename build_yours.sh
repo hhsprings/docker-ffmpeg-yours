f43d9d94d1becf26fb1214b0a36ee5debc3b62cd
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-03 01:00:05 +0900

    Update build_yours.sh

--- build_yours.sh
+++ build_yours.sh
@@ -6,26 +6,29 @@ export _VER_YOURS=0.3
 export _BUILDPACKDEPS_TAG_FOR_LATEST=22.10
 export _FFMPEG_VERSION=4.4.2
 export _OPENCV_VERSION=3.4.15
+cd yours
 for _BUILDPACKDEPS_TAG in 22.04 22.10 ; do
-(
+
     export _FFMPEG_EXTRA_VERSION_SUFFIX=${_YOU}${_VER_YOURS}
-    cd yours
     docker build -f Dockerfile -t ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_OPENCV_VERSION}-${_BUILDPACKDEPS_TAG} \
            --build-arg _BUILDPACKDEPS_TAG=${_BUILDPACKDEPS_TAG} \
            --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
            --build-arg _OPENCV_VERSION=${_OPENCV_VERSION} \
            --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
+           .
+    test $? -ne 0 || exit $?
     if \! -z "${__dopush}" ; then
         docker push ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_OPENCV_VERSION}-${_BUILDPACKDEPS_TAG}
-    fi
-           .
+        test $? -ne 0 || exit $?
+    fi           .
     if test "${_BUILDPACKDEPS_TAG}" = "${_BUILDPACKDEPS_TAG_FOR_LATEST}" ; then
         docker tag \
                ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_OPENCV_VERSION}-${_BUILDPACKDEPS_TAG} \
                ${_YOU}/ffmpeg-yours:latest
         if \! -z "${__dopush}" ; then
             docker push ${_YOU}/ffmpeg-yours:latest
+            test $? -ne 0 || exit $?
         fi
     fi
-)
+
 done
