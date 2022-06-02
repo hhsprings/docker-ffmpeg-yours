bd0ba1625c5f8f95dca7c9e10c3820d0752c47ee
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-02 21:35:51 +0900

    Create build_min.sh

---
+++ build_min.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+export _YOU=${_YOU:-hhsprings}
+export _VER_MIN=${_VER_MIN:-0.3}
+export _VER_YOURS=${_VER_YOURS:-0.3}
+export _BUILDPACKDEPS_TAG_FOR_LATEST=22.10
+export _FFMPEG_VERSION=4.4.2
+export _OPENCV_VERSION=3.4.15
+for _BUILDPACKDEPS_TAG in 22.04 22.10 ; do
+(
+    export _FFMPEG_EXTRA_VERSION_SUFFIX=${_YOU}${_VER_MIN}-min
+    cd min
+    docker build -f Dockerfile -t ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
+           --build-arg _BUILDPACKDEPS_TAG=${_BUILDPACKDEPS_TAG} \
+           --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
+           --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
+           .
+    docker tag \
+           ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
+           ${_YOU}/ffmpeg-yours-min:latest-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}
+    if test "${_BUILDPACKDEPS_TAG}" = "${_BUILDPACKDEPS_TAG_FOR_LATEST}" ; then
+        docker tag \
+               ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
+               ${_YOU}/ffmpeg-yours-min:latest
+    fi
+)
+done
