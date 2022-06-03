2acb7dd891873e8b19410a8fc0fbc6a67fa4af84
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-03 22:57:49 +0900

    Update build_min.sh

--- build_min.sh
+++ build_min.sh
@@ -14,31 +14,32 @@ for _BUILDPACKDEPS_TAG in \
     #
     if (docker run ${__PULLNEVER} -t \
                ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
-               ffmpeg --version 2>&1) > /dev/null ; then
+               ffmpeg --version 2>&1) ; then
         :
     else
-        docker build -f Dockerfile -t ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
-               --build-arg _BUILDPACKDEPS_TAG=${_BUILDPACKDEPS_TAG} \
-               --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
-               --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
-               . || exit $?
-        docker tag \
-               ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
-               ${_YOU}/ffmpeg-yours-min:latest-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}
-        if test "z${__dopush}" = "z1" ; then
-            (docker push ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} && \
-                 docker push ${_YOU}/ffmpeg-yours-min:latest-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}) || exit $?
-        fi
-        if test "${_BUILDPACKDEPS_TAG}" = "${_BUILDPACKDEPS_TAG_FOR_LATEST}" ; then
-            docker tag \
-                   ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
-                   ${_YOU}/ffmpeg-yours-min:latest
-            if test "z${__dopush}" = "z1" ; then
-                docker push ${_YOU}/ffmpeg-yours-min:latest || exit $?
-            fi
-        fi
-        if test $? -eq 0 ; then
-            docker run -t --rm ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} ffmpeg -version
+        :
+        #docker build -f Dockerfile -t ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
+        #       --build-arg _BUILDPACKDEPS_TAG=${_BUILDPACKDEPS_TAG} \
+        #       --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
+        #       --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
+        #       . || exit $?
+        #docker tag \
+        #       ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
+        #       ${_YOU}/ffmpeg-yours-min:latest-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}
+        #if test "z${__dopush}" = "z1" ; then
+        #    (docker push ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} && \
+        #         docker push ${_YOU}/ffmpeg-yours-min:latest-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}) || exit $?
+        #fi
+        #if test "${_BUILDPACKDEPS_TAG}" = "${_BUILDPACKDEPS_TAG_FOR_LATEST}" ; then
+        #    docker tag \
+        #           ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
+        #           ${_YOU}/ffmpeg-yours-min:latest
+        #    if test "z${__dopush}" = "z1" ; then
+        #        docker push ${_YOU}/ffmpeg-yours-min:latest || exit $?
+        #    fi
+        #fi
+        #if test $? -eq 0 ; then
+        #    docker run -t --rm ${_YOU}/ffmpeg-yours-min:${_VER_MIN}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} ffmpeg -version
         fi
     fi
 done
