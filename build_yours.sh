1c6bd13d2e90d0e49f2934a5a712eb9c57c1a8e8
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-05 01:15:37 +0900

    Update build_yours.sh

--- build_yours.sh
+++ build_yours.sh
@@ -4,7 +4,6 @@ export _YOU=${_YOU:-hhsprings}
 export _VER_YOURS=0.6
 export _BUILDPACKDEPS_TAG_FOR_LATEST=22.10
 export _FFMPEG_VERSION=4.4.2
-export _OPENCV_VERSION=3.4.15
 cd yours
 for _BUILDPACKDEPS_TAG in 22.04 22.10 ; do
 
@@ -12,7 +11,6 @@ for _BUILDPACKDEPS_TAG in 22.04 22.10 ; do
     docker build -f Dockerfile -t ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
            --build-arg _BUILDPACKDEPS_TAG=${_BUILDPACKDEPS_TAG} \
            --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
-           --build-arg _OPENCV_VERSION=${_OPENCV_VERSION} \
            --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
            . || exit $?
     if test "z${__dopush}" = "z1" ; then
