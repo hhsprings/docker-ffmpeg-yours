f72ec5770fefad86238c34edea4cc2b7ec27dfac
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-05 22:07:22 +0900

    Update _apt_install.sh

--- yours/_apt_install.sh
+++ yours/_apt_install.sh
@@ -1,10 +1,19 @@
 #! /bin/sh
 # $1: target directory
 # $2: apt package(s)
-# $3: a flag of configure
+# $3: alternative package(s) if $2 is not available
+# $4: a flag of configure
 if apt-get install -yq --no-install-recommends ${2} ; then
-    grep -- "${3}" ${1}/_enable_if_available > /dev/null || \
-        echo ${3} >> ${1}/_enable_if_available
+    grep -- "${4}" ${1}/_enable_if_available > /dev/null || \
+        echo ${4} >> ${1}/_enable_if_available
 else
-    true
+    if test "z$3" != "z" ; then
+        if apt-get install -yq --no-install-recommends ${3} ; then
+            grep -- "${4}" ${1}/_enable_if_available > /dev/null || \
+                echo ${4} >> ${1}/_enable_if_available
+        else
+            true
+    else
+        true
+    fi
 fi
