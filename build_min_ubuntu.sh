ddb8112adcbf9db5e27ae1515fa31670816016da
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-06 23:10:37 +0900

    Update build_min_ubuntu.sh

--- build_min_ubuntu.sh
+++ build_min_ubuntu.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-sh build_min_tmpl.sh "16.04 18.04 20.04 21.10 22.04 22.10" "$@"
+#sh build_min_tmpl.sh "16.04 18.04 20.04 21.10 22.04 22.10" "$@"
+sh build_min_tmpl.sh "22.04 22.10" "$@"
