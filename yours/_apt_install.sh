0b6d0da3099f8178af130b7bd83c4a4b4fa7f687
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-06 15:06:24 +0900

    Update _apt_install.sh

--- yours/_apt_install.sh
+++ yours/_apt_install.sh
@@ -3,13 +3,20 @@
 # $2: apt package(s)
 # $3: alternative package(s) if $2 is not available
 # $4: a flag of configure
+
+# note that i want to log requests of "apt-get install", not actually installed.
+logfn=$0.req-log
 if apt-get install -yq --no-install-recommends ${2} ; then
     grep -- "${4}" ${1}/_enable_if_available > /dev/null || \
         echo ${4} >> ${1}/_enable_if_available
+        echo $2 | sed 's@  *@\
+@g' | sed 's@-dev$@@' >> ${logfn}
 elif test "z$3" != "z" ; then
     if apt-get install -yq --no-install-recommends ${3} ; then
         grep -- "${4}" ${1}/_enable_if_available > /dev/null || \
             echo ${4} >> ${1}/_enable_if_available
+        echo $3 | sed 's@  *@\
+@g' | sed 's@-dev$@@' >> ${logfn}
     else
         true
     fi
