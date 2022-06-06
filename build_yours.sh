41c68aca88a32900143211d90dafaee0e95479b2
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-06 16:43:22 +0900

    Update build_yours.sh

--- build_yours.sh
+++ build_yours.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 export __dopush=$(test "z${1}" = "z--push" && echo 1)
 export _YOU=${_YOU:-hhsprings}
-export _VER_YOURS=0.7
+export _VER_YOURS=0.8
 export _BUILDPACKDEPS_TAG_FOR_LATEST=22.10
 export _FFMPEG_VERSION=4.4.2
 export _OPENCV_VERSION=3.4.15
