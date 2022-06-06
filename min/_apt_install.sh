ffd1cf31fb47ce5400a7c341cace7852f3213598
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-06 19:04:51 +0900

    Update _apt_install.sh

--- min/_apt_install.sh
+++ min/_apt_install.sh
@@ -7,13 +7,13 @@
 # note that i want to log requests of "apt-get install", not actually installed.
 destdir="`dirname \"$0\"`"
 logfn=$0.req-log
-if apt-get install -yq --no-install-recommends ${2} ; then
+if apt-get install -y -qq --no-install-recommends ${2} ; then
     grep -- "${4}" ${destdir}/_enable_if_available > /dev/null || \
         echo "${1}:"${4} >> ${destdir}/_enable_if_available
         echo $2 | sed 's@  *@\
 @g' | sed 's@-dev$@@' >> ${logfn}
 elif test "z$3" != "z" ; then
-    if apt-get install -yq --no-install-recommends ${3} ; then
+    if apt-get install -y -qq --no-install-recommends ${3} ; then
         grep -- "${4}" ${destdir}/_enable_if_available > /dev/null || \
             echo "${1}:"${4} >> ${destdir}/_enable_if_available
         echo $3 | sed 's@  *@\
