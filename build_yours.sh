388cb14a181ac87c973b17e4dde350a80bd988df
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-09 22:10:45 +0900

    Update build_yours.sh

--- build_yours.sh
+++ build_yours.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-export __dopush=$(test "z${1}" = "z--push" && echo 1)
+export __push=$(if test "z${1:-''}" = "z--push" ; then echo true ; else echo false ; fi)
 export _YOU=${_YOU:-hhsprings}
 export _VER_YOURS=0.8.1
 export _BUILDPACKDEPS_TAG_FOR_LATEST=22.10
@@ -10,25 +10,22 @@ cd yours
 for _BUILDPACKDEPS_TAG in 22.10 22.04 ; do
 
     export _FFMPEG_EXTRA_VERSION_SUFFIX=${_YOU}${_VER_YOURS}
-    docker build -f Dockerfile -t ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
+    r="${_YOU}/ffmpeg-yours"
+    t="${_VER_YOURS}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG}"
+    t_opt="-t ${r}:${t}"
+    if test "${_BUILDPACKDEPS_TAG}" = "${_BUILDPACKDEPS_TAG_FOR_LATEST}" ; then
+        t_opt="${t_opt} -t ${r}:latest"
+    fi
+    docker buildx build -f Dockerfile \
+           ${t_opt} \
            --build-arg _BUILDPACKDEPS_TAG=${_BUILDPACKDEPS_TAG} \
            --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
            --build-arg _OPENCV_VERSION=${_OPENCV_VERSION} \
            --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
            --platform linux/amd64 \
+           -o type=image,push=${__push} \
            . || exit $?
-    if test "z${__dopush}" = "z1" ; then
-        docker push ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} || exit $?
-    fi
-    if test "${_BUILDPACKDEPS_TAG}" = "${_BUILDPACKDEPS_TAG_FOR_LATEST}" ; then
-        docker tag \
-               ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} \
-               ${_YOU}/ffmpeg-yours:latest
-        if test "z${__dopush}" = "z1" ; then
-            docker push ${_YOU}/ffmpeg-yours:latest || exit $?
-        fi
-    fi
     if test $? -eq 0 ; then
-        docker run -t --rm ${_YOU}/ffmpeg-yours:${_VER_YOURS}-${_FFMPEG_VERSION}-${_BUILDPACKDEPS_TAG} ffmpeg -version
+        docker run -t --rm ${r}:${t} ffmpeg -version
     fi
 done
