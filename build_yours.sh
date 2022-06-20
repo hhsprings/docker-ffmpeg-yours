4c43164bdd1db9a5842588c670531d6762d06ab2
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-20 13:36:23 +0900

    Update build_yours.sh

--- build_yours.sh
+++ build_yours.sh
@@ -1,10 +1,9 @@
 #! /bin/sh
 export __push=$(if test "z${1:-''}" = "z--push" ; then echo true ; else echo false ; fi)
 export _YOU=${_YOU:-hhsprings}
-export _VER_YOURS=0.8.10
+export _VER_YOURS=0.8.11
 export _BUILDPACKDEPS_TAG_FOR_LATEST=22.10
 export _FFMPEG_VERSION=4.4.2
-export _OPENCV_VERSION=3.4.15
 
 cd yours
 for _BUILDPACKDEPS_TAG in 22.10 22.04 ; do
@@ -21,7 +20,6 @@ for _BUILDPACKDEPS_TAG in 22.10 22.04 ; do
            ${t_opt} \
            --build-arg _BUILDPACKDEPS_TAG=${_BUILDPACKDEPS_TAG} \
            --build-arg _FFMPEG_VERSION=${_FFMPEG_VERSION} \
-           --build-arg _OPENCV_VERSION=${_OPENCV_VERSION} \
            --build-arg _FFMPEG_EXTRA_VERSION_SUFFIX=${_FFMPEG_EXTRA_VERSION_SUFFIX} \
            --platform linux/amd64 \
            -o type=image,push=${__push} \
