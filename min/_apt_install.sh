03fb357d1d046ef35afd606f99f10fe48a8c2be5
Hiroaki Itoh <xwhhsprings@gmail.com>
2022-06-23 17:47:19 +0900

    Update _apt_install.sh

--- min/_apt_install.sh
+++ min/_apt_install.sh
@@ -7,20 +7,35 @@
 # note that i want to log requests of "apt-get install", not actually installed.
 destdir="`dirname \"$0\"`"
 logfn=$0.req-log
-if apt-get install -y -qq --no-install-recommends ${2} ; then
-    grep -- "${4}" ${destdir}/_enable_if_available > /dev/null || \
-        echo "${1}:"${4} >> ${destdir}/_enable_if_available
-        echo $2 | sed 's@  *@\
-@g' >> ${logfn}
-elif test "z$3" != "z" ; then
-    if apt-get install -y -qq --no-install-recommends ${3} ; then
-        grep -- "${4}" ${destdir}/_enable_if_available > /dev/null || \
-            echo "${1}:"${4} >> ${destdir}/_enable_if_available
-        echo $3 | sed 's@  *@\
-@g' >> ${logfn}
-    else
-        true
-    fi
+
+_pkg_exists() {
+    for p in ${1} ; do
+        apt-cache policy $p | grep Candidate || return 1
+    done
+    return 0
+}
+
+#
+# The package may not exist because it depends on the distro.
+# In that case, "Non-existence" of "$2" or "$3" should not be
+# regarded as an error. On the other hand, if we know it exists
+# and its installation fails, we should treat it as an error.
+# Network communication timeouts are the most likely error,
+# and in fact this has caused me to make a mistake in the
+# --enable-sdl2 related decision.
+#
+if _pkg_exists "${2}" ; then
+    apt-get install -y -qq --no-install-recommends ${2} && \
+        (grep -- "${4}" ${destdir}/_enable_if_available > /dev/null || \
+                echo "${1}:"${4} >> ${destdir}/_enable_if_available ; \
+         echo $2 | sed 's@  *@\
+@g' >> ${logfn})
+elif test "z$3" != "z" && _pkg_exists "${3}" ; then
+    apt-get install -y -qq --no-install-recommends ${3} && \
+        (grep -- "${4}" ${destdir}/_enable_if_available > /dev/null || \
+                echo "${1}:"${4} >> ${destdir}/_enable_if_available ; \
+         echo $3 | sed 's@  *@\
+@g' >> ${logfn})
 else
     true
 fi
